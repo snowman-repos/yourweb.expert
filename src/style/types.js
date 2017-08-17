679a455beb602db30bc1bc0d25ef00ad1b4c3534
Darryl Snow <darryl@yourweb.expert>
2017-08-17 22:24:26 +0800

    add icon sprite

---
+++ src/style/types.js
@@ -0,0 +1,6 @@
+export const CALENDAR_CREATE = 'calendar_create';
+export const CALENDAR_SAVE = 'calendar_save';
+
+export const LOGIN_USER = 'login_user';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
